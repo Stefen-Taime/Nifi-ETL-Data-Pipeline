cda128ff15713bd721557214690449c8c63b8ffe
Stefen <61303554+Stefen-Taime@users.noreply.github.com>
2022-04-01 13:05:29 -0400

    Update init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE demo;
+CREATE DATABASE aviation;
+
+CREATE TABLE flights ( 
+  id SERIAL PRIMARY KEY,
+  fligh_date DATE NOT NULL,
+  departure_airport VARCHAR(50) DEFAULT NULL,
+  departure_timezone VARCHAR(50)  DEFAULT NULL,
+  departure_IATA VARCHAR(50)  DEFAULT NULL,
+  arrival_airport VARCHAR(50)  DEFAULT NULL,
+  arrival_timezone VARCHAR(50)  DEFAULT NULL,
+  arrival_IATA VARCHAR(50)  DEFAULT NULL,
+  airline_name VARCHAR(50)  DEFAULT NULL );
